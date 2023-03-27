9b21326b1a0897ca206937eb5c93d5b43b0963db
Ryan Yogan <ryanyogan@gmail.com>
2023-03-27 14:53:46 -0500

    feat: project display

--- src/app/(dashboard)/project/[id]/page.tsx
+++ src/app/(dashboard)/project/[id]/page.tsx
@@ -1,5 +1,31 @@
-function Project() {
-  return <div>Project</div>;
+import { TaskCard } from "@/components/task-card";
+import { getUserFromCookie } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { cookies } from "next/headers";
+
+async function getData(id: string) {
+  const user = await getUserFromCookie(cookies());
+  const project = await db.project.findFirst({
+    where: { id, userId: user?.id },
+    include: {
+      tasks: true,
+    },
+  });
+
+  return { project };
 }
 
-export default Project;
+export default async function ProjectPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { project } = await getData(params.id);
+
+  return (
+    <div className="h-full overflow-y-auto pr-6 w-full">
+      {/** @ts-ignore */}
+      <TaskCard tasks={project?.tasks} title={project?.name} />
+    </div>
+  );
+}
